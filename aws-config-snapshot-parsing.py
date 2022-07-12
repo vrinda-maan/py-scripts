ab8fa34867b1b9ee333df878e14fbeb2773caff7
vrinda.M <vrinda.maan99@gmail.com>
2022-07-12 15:02:36 +0530

    Trimmed comments

--- aws-config-snapshot-parsing.py
+++ aws-config-snapshot-parsing.py
@@ -6,12 +6,13 @@ import pandas as pd
 data = []
 nan_value = float("NaN")
 
-# Stage 1 - Read JSON files from a folder & attach that to a python list for processing
+# Stage 1 - Read AWS Config Snapshots in JSON format from a folder & attach that to a python list for processing
+# Refer to https://docs.aws.amazon.com/config/latest/developerguide/view-manage-resource-console.html for more details on AWS Config snapshots
 for file_name in glob('json/*.json'):
     with open(file_name) as f:
         data.append(json.load(f))
 
-# Stage 2 - Flatten JSON into Pandas Dataframe 
+# Stage 2 - Flatten AWS Config Snapshot JSON into Pandas Dataframe 
 df = pd.json_normalize(data, record_path=['configurationItems'])
 
 # Sort Dataframe by resource-type (this is optional. have not tested for performance implications of sorting)
@@ -21,12 +22,6 @@ df = pd.json_normalize(data, record_path=['configurationItems'])
 grp = df.groupby('resourceType')
 
 # Stage 4 - Loop through resource groups and convert those into CSV files
-'''
-Notes 
-- Loop operation can be multi-threaded. a good option for handling it - concurrent.futures python library
-- Dataframe can be saved as parquet file insteat of csv (ref: https://pandas.pydata.org/pandas-docs/version/1.1/reference/api/pandas.DataFrame.to_parquet.html)
-'''
-
 for name, group in grp:
     group.replace("", nan_value, inplace=True)
     group.dropna(how='all', axis=1, inplace=True)
